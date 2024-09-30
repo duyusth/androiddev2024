bcba74069bfa1d8d875c48ee744ddbc66c737bed
duyusth <duynd.ba12-062@st.usth.edu.vn>
2024-09-30 09:54:16 +0700

    p3

---
+++ USTHWeather/app/src/main/java/com/example/usthweather/ForecastFragment.java
@@ -0,0 +1,31 @@
+package com.example.usthweather;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ForecastFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        // Set màu nền cho Fragment
+        view.setBackgroundColor(Color.parseColor("#20FF0000"));  // Bạn có thể thay đổi các mã màu khác
+        return view;
+    }
+}
