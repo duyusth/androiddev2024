b1a83891b98aca4a689cb418efc4b2b721616d87
duyusth <duynd.ba12-062@st.usth.edu.vn>
2024-09-30 10:28:26 +0700

    p6

--- USTHWeather/app/src/main/java/com/example/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/com/example/usthweather/WeatherActivity.java
@@ -1,21 +1,9 @@
 package com.example.usthweather;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import android.os.Bundle;
-import android.util.Log;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 public class WeatherActivity extends AppCompatActivity {
@@ -23,12 +11,12 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather); // Đảm bảo layout có FrameLayout với id là 'container'
+        setContentView(R.layout.activity_weather);
 
-        // Thêm ForecastFragment vào Activity
-        ForecastFragment forecastFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.container, forecastFragment) // 'container' là ID của FrameLayout trong layout
-                .commit();
+        // Thêm WeatherFragment vào giao diện
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_weather, new WeatherFragment());
+        transaction.replace(R.id.fragment_forecast, new ForecastFragment());
+        transaction.commit();
     }
 }
---
+++ USTHWeather/app/src/main/java/com/example/usthweather/WeatherFragment.java
@@ -0,0 +1,21 @@
+package com.example.usthweather;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+
+public class WeatherFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_weather, container, false);
+
+        return view;
+    }
+}
+
