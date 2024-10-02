e103a8123199355c65015a7d9ea6c0c98aa12f7d
duyusth <duynd.ba12-062@st.usth.edu.vn>
2024-10-02 14:47:36 +0700

    prac15

--- USTHWeather/app/src/main/java/com/example/usthweather/WeatherFragment.java
+++ USTHWeather/app/src/main/java/com/example/usthweather/WeatherFragment.java
@@ -1,21 +1,71 @@
 package com.example.usthweather;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class WeatherFragment extends Fragment {
 
+    private ImageView imageView;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_weather, container, false);
+        imageView = view.findViewById(R.id.weather_icon);
+
+        new DownloadImageTask().execute("https://i.ytimg.com/vi/SbCR8khRTwQ/maxresdefault.jpg");
 
         return view;
     }
-}
 
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getActivity(), "Downloading USTH logo...", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String imageUrl = urls[0];
+            Bitmap bitmap = null;
+            try {
+                URL url = new URL(imageUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(input);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            if (result != null) {
+                imageView.setImageBitmap(result);
+                Toast.makeText(getActivity(), "Logo downloaded ", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getActivity(), "Failed ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
